e287ab92be5e9a6032a64484612487979740d8ae
site <119048961+jkdfrance@users.noreply.github.com>
2023-07-12 17:02:46 +0200

    Create index.php
    creation premier commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar">
+        
+        <!--<img src="images/premier.png" class="Logo" alt="logo" width:00px>-->
+        <a href="#" class="Logo">IMG Vincent<br> PARROT</a>
+        
+        <div class="nav-links">
+            <ul>
+                
+                <!--<li class="active"><a href="#">Acceuil</a></li>-->
+                <li><a href="#"></a>Révisions</li>
+                <li><a href="#"></a>Dépannage</li>
+                <li><a href="#"></a>Occasions</li>
+                <li><a href="#"></a>Contact</li>
+            </ul>
+            
+                </ul>
+        </div class="menu">
+            <!--<img src="images/bars.png"  alt="bars" class="barre">-->
+            <a href="#" class="btn btn-primary" role="button">Connexion</a>
+        </div>
+        
+        </nav>
+    </header>
+
+    <img src="images/bugatti.jpg" class="fond" alt="car">
+   
+</body>
+ 
+<!--<script>
+    const menuHamburger =  document.queryselector(".barre");
+    const navLinks = document.queryselector(".nav-links");
+    
+    menu.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu')
+    });
+</script>-->
+</html>
